cbcaf8deec9c13ec14ed02ec9010d894ae763775
amiraliir <rahimi.amirali@ut.ac.ir>
2022-04-02 07:27:33 -0400

    forgot debugging

--- emtiazi.cpp
+++ emtiazi.cpp
@@ -11,56 +11,58 @@ long int Sum(vector<int> v) {
 }
 
 
-void solve(int m, int n, long int& harvest){
-    long int sum = 0;
+int solve(int row_size, int col_size,vector<vector<int>> farm){
+    int result = 0;
+    int sum = 0;
     int max_row = 0;
-    int maxColumn = 0;
+    int max_column = 0;
     int row = 0;
     int column = 0;
 
     for(int move = 0 ; move < 4 ; move++) {
-        vector<long int> row_sums;
+        vector<int> row_sums;
 
-        for(int i = 0 ; i < m ; i++){
+        for(int i = 0 ; i < row_size ; i++){
             sum = Sum(farm[i]);
             row_sums.push_back(sum);
             sum = 0;
         }
 
-        for(int i = 0 ; i < m ; i++){
+        for(int i = 0 ; i < row_size ; i++){
             if (row_sums[i] > max_row) {
                 max_row = row_sums[i];
                 row = i;
             }
         }
 
-        for(int j = 0 ; j < n ; j++){
-            for(int i = 0 ; i < m ; i++){
+        for(int j = 0 ; j < col_size ; j++){
+            for(int i = 0 ; i < row_size ; i++){
                 sum += farm[i][j];
             }
-            if(sum > maxColumn){
-                maxColumn = sum;
+            if(sum > max_column){
+                max_column = sum;
                 column = j;
             }
             sum = 0;
         }
-        if(maxColumn > max_row){
-            harvest += maxColumn;
-            for(int i = 0 ; i < m ; i++){
+        if(max_column > max_row){
+            result += max_column;
+            for(int i = 0 ; i < row_size ; i++){
                 farm[i][column] = 0;
             }
         }
         else{
-            harvest += max_row;
-            for(int j = 0 ; j < n ; j++){
+            result += max_row;
+            for(int j = 0 ; j < col_size ; j++){
                 farm[row][j] = 0;
             }
         }
         row = 0;
         column = 0;
-        maxColumn = 0;
+        max_column = 0;
         max_row = 0;
     }
+    return result;
 }
 
 vector<vector<int>> get_farm_map(int row_size, int col_size){
@@ -81,8 +83,9 @@ int main() {
     cin >> row_size >> col_size;
     vector<vector<int>> farm;
     farm = get_farm_map(row_size, col_size);
-    solve(row_size, col_size);
-    cout << harvest;
+    int max_harvest_amount;
+    max_harvest_amount = solve(row_size, col_size, farm);
+    cout << max_harvest_amount;
     return 0;
-    cout << "Hello there! I'm unreachable!" << endl; 
+    cout << "Hello there! I'm unreachable!" << endl;
 }
